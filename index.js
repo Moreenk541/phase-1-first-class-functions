bedcc5fc9cf241c662c6958b75ee68ad4e15e8f7
moreen karimi <moreenk541@gmail.com>
2025-03-12 11:42:59 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback){
+    callback()
+} 
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("I am a named function!");
+    }
+    return namedFunction;
+}
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("anonymous function!");
+    };
+}
+
+
+const myFunction = returnsAnAnonymousFunction();
+myFunction();  
+
